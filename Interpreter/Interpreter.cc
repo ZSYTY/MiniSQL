0b5a1a94fee96f695cb8ed0f1544039b6c978ee1
chen-456 <chen.925@hotmail.com>
2020-06-14 17:07:57 +0800

    Basic REPL implementation

---
+++ Interpreter/Interpreter.cc
@@ -0,0 +1,67 @@
+#include "Interpreter.h"
+
+#include <algorithm>
+#include <iostream>
+
+static constexpr auto GREETINGS = "MiniSQL Interpreter v0.1\n\n";
+
+Interpreter::Interpreter() {}
+
+Interpreter::~Interpreter() {}
+
+void Interpreter::run()
+{
+    std::cout << GREETINGS;
+    std::string sql;    // buffer for SQL statements
+
+    while(true) {
+        if(sql.empty())
+            std::cout << "MiniSQL > ";
+        else
+            std::cout << "        > ";
+
+        std::string input;
+        std::getline(std::cin, input);
+
+        {
+            // check if the input is an interpreter directive
+            // e.g. ".exit", ".break"
+            auto it = std::find_if(input.begin(), input.end(), [](int ch) {
+                return !std::isspace(ch);
+            });
+            if(it == input.end())   // the input is empty
+                continue;
+            if(*it == '.') {
+                auto end = std::find_if(it + 1, input.end(), [](int ch) {
+                    return std::isspace(ch);
+                });
+                std::string directive(it, end);
+                if(directive == ".b" || directive == ".break") {
+                    sql.clear();
+                    continue;
+                } else if(directive == ".e" || directive == ".exit" || directive == ".q"
+                    || directive == ".quit") {
+                    std::cout << "Bye\n\n";
+                    return;
+                } else {
+                    std::cout << "Error: unknown interpreter directive \"" << directive << '\"'
+                        << std::endl;
+                    sql.clear();
+                    continue;
+                }
+            }
+        }
+
+        {
+            // else, append the input to SQL buffer
+            sql += input + ' ';
+            if(sql.find(';') != std::string::npos) {
+                // execute the statement
+                // currently, only one statement per line is supported
+                execute_sql(sql);
+                std::cout << std::endl;
+                sql.clear();
+            }
+        }
+    }
+}
--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -1,23 +1,27 @@
 /*
  * @Author: Tianyu You 
  * @Date: 2020-05-24 16:28:51 
- * @Last Modified by: Tianyu You
- * @Last Modified time: 2020-05-24 16:40:57
+ * @Last Modified by: Chen Yu
+ * @Last Modified time: 2020-06-14 17:07
  */
 
 #ifndef MINISQL_INTERPRETER_H
 #define MINISQL_INTERPRETER_H
 
+#include <string>
+
 class Interpreter
 {
-private:
-    /* data */
 public:
-    Interpreter(/* args */);
+    Interpreter();
     ~Interpreter();
     
-    /* run a SQL interpreter */
+    /* run an SQL interpreter */
     void run();
+
+private:
+    /* execute an SQL statement */
+    void execute_sql(const std::string &statement);
 };
 
 #endif
