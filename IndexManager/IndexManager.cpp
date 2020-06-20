ff5da75f1ea3e4af14ba1ab0a8b470c7c540430c
ZSYTY <793533676@qq.com>
2020-06-21 00:10:13 +0800

    feat: IndexManager using B+ tree

---
+++ IndexManager/IndexManager.cpp
@@ -0,0 +1,214 @@
+/*
+ * @Author: Tianyu You 
+ * @Date: 2020-06-10 16:46:54 
+ * @Last Modified by: Tianyu You
+ * @Last Modified time: 2020-06-20 02:05:42
+ */
+
+#include "IndexManager.h"
+
+MiniSQL::SqlValueBaseType IndexManager::getColomnType(const std::string &tableName, const std::string &columnName) {
+    tableInfo = catalogManager->getTableInfo(tableName);
+    bool found = false;
+    for (auto &item : tableInfo.indexes) {
+        if (item.columnName == columnName) {
+            indexInfo = item;
+            found = true;
+        }
+    }
+    if (! found) {
+        throw std::runtime_error("No such index");
+    }
+    for (int i = 0; i < tableInfo.columnName.size(); i++) {
+        if (tableInfo.columnName[i] == columnName) {
+            columnIdx = i;
+            return tableInfo.columnType[i].type;
+        }
+    }
+    throw std::runtime_error("No such column");
+}
+
+intTree& IndexManager::getIntTree() {
+    auto key = std::make_pair(indexInfo.tableName, indexInfo.columnName);
+    if (intMap.count(key)) {
+        return intTreeList[intMap[key]];
+    } else {
+        intTreeList.push_back(intTree(bufferManager, indexInfo, tableInfo));
+        intMap[key] = intTreeList.size() - 1;
+        return intTreeList.back();
+    }
+}
+
+charTree& IndexManager::getCharTree() {
+    auto key = std::make_pair(indexInfo.tableName, indexInfo.columnName);
+    if (charMap.count(key)) {
+        return charTreeList[charMap[key]];
+    } else {
+        charTreeList.push_back(charTree(bufferManager, indexInfo, tableInfo));
+        charMap[key] = charTreeList.size() - 1;
+        return charTreeList.back();
+    }
+}
+
+floatTree& IndexManager::getFloatTree() {
+    auto key = std::make_pair(indexInfo.tableName, indexInfo.columnName);
+    if (floatMap.count(key)) {
+        return floatTreeList[floatMap[key]];
+    } else {
+        floatTreeList.push_back(floatTree(bufferManager, indexInfo, tableInfo));
+        floatMap[key] = floatTreeList.size() - 1;
+        return floatTreeList.back();
+    }
+}
+
+IndexManager::~IndexManager() {
+    
+}
+
+bool IndexManager::buildIndex(const IndexInfo &index) {
+    // TODO:
+    auto T = getColomnType(index.tableName, index.columnName);
+    switch (T) {
+        case MiniSQL::SqlValueBaseType::MiniSQL_int:
+            getIntTree().build();
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_char:
+            getCharTree().build();
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_float:
+            getFloatTree().build();
+            break;
+    }
+    return true;
+}
+
+bool IndexManager::dropIndex(const std::string &indexName) {
+    auto table = catalogManager->getTableInfo(indexName);
+    bufferManager->removeFile(indexName + ".index");
+    return true;
+}
+
+int IndexManager::search(const std::string &tableName, const std::string &columnName, const SqlValue &value) {
+    auto T = getColomnType(tableName, columnName);
+    switch (T) {
+        case MiniSQL::SqlValueBaseType::MiniSQL_int:
+            return getIntTree().select(value);
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_char:
+            return getCharTree().select(value);
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_float:
+            return getFloatTree().select(value);
+            break;
+    }
+}
+
+int IndexManager::searchNext(const std::string &tableName, const std::string &columnName) {
+    auto T = getColomnType(tableName, columnName);
+    switch (T) {
+        case MiniSQL::SqlValueBaseType::MiniSQL_int:
+            return getIntTree().selectNext();
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_char:
+            return getCharTree().selectNext();
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_float:
+            return getFloatTree().selectNext();
+            break;
+    }
+}
+
+int IndexManager::serachEqual(const std::string &tableName, const std::string &columnName, const Tuple &tuple) {
+    auto T = getColomnType(tableName, columnName);
+    SqlValue value = tuple[columnIdx];
+    Tuple curTuple;
+    int cur = -1;
+    switch (T) {
+        case MiniSQL::SqlValueBaseType::MiniSQL_int:
+            cur = getIntTree().select(value);
+            while (cur != -1) {
+                curTuple = getIntTree().getTuple(cur);
+                if (curTuple == tuple) { // need to override operator
+                    return cur;
+                } else {
+                    if (curTuple[columnIdx] != value) {
+                        break;
+                    }
+                    cur = getIntTree().selectNext();
+                }
+            }
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_char:
+            cur = getCharTree().select(value);
+            while (cur != -1) {
+                curTuple = getCharTree().getTuple(cur);
+                if (curTuple == tuple) { // need to override operator
+                    return cur;
+                } else {
+                    if (curTuple[columnIdx] != value) {
+                        break;
+                    }
+                    cur = getCharTree().selectNext();
+                }
+            }
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_float:
+            cur = getFloatTree().select(value);
+            while (cur != -1) {
+                curTuple = getFloatTree().getTuple(cur);
+                if (curTuple == tuple) { // need to override operator
+                    return cur;
+                } else {
+                    if (curTuple[columnIdx] != value) {
+                        break;
+                    }
+                    cur = getFloatTree().selectNext();
+                }
+            }
+            break;
+    }
+    return -1;
+}
+
+int IndexManager::searchHead(const std::string &tableName, const std::string &columnName) {
+    auto T = getColomnType(tableName, columnName);
+    switch (T) {
+        case MiniSQL::SqlValueBaseType::MiniSQL_int:
+            return getIntTree().selectHead();
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_char:
+            return getCharTree().selectHead();
+            break;
+        case MiniSQL::SqlValueBaseType::MiniSQL_float:
+            return getFloatTree().selectHead();
+            break;
+    }
+}
+
+bool IndexManager::insertKey(const std::string &tableName, const Tuple &tuple, int offset) {
+    bool isInserted = false;
+    tableInfo = catalogManager->getTableInfo(tableName);
+    for (int i = 0; i < tableInfo.columnName.size(); i++) {
+        for (auto &item : tableInfo.indexes) {
+            if (item.columnName == tableInfo.columnName[i]) {
+                indexInfo = item;
+                columnIdx = i;
+                auto &T = tableInfo.columnType[columnIdx].type;
+                switch (T) {
+                    case MiniSQL::SqlValueBaseType::MiniSQL_int:
+                        getIntTree().insertVal(tuple[columnIdx], offset);
+                        break;
+                    case MiniSQL::SqlValueBaseType::MiniSQL_char:
+                        getCharTree().insertVal(tuple[columnIdx], offset);
+                        break;
+                    case MiniSQL::SqlValueBaseType::MiniSQL_float:
+                        getFloatTree().insertVal(tuple[columnIdx], offset);
+                        break;
+                }
+                isInserted = true;
+                break;
+            }
+        }
+    }
+    return isInserted;
+}
--- IndexManager/IndexManager.h
+++ IndexManager/IndexManager.h
@@ -2,39 +2,68 @@
  * @Author: Tianyu You 
  * @Date: 2020-05-24 18:21:29 
  * @Last Modified by: Tianyu You
- * @Last Modified time: 2020-05-24 23:22:03
+ * @Last Modified time: 2020-06-20 01:58:24
  */
 
 #ifndef MINISQL_INDEX_MANAGER_H
 #define MINISQL_INDEX_MANAGER_H
 
+#include <map>
+#include <vector>
+
 #include "../BufferManager/BufferManager.h"
+#include "../CatalogManager/CatalogManager.h"
 #include "../Common/Common.h"
+#include "BPlus.h"
 
 using namespace MiniSQL;
+
+typedef BPlusTree<MiniSQL::SqlValueBaseType::MiniSQL_int> intTree;
+typedef BPlusTree<MiniSQL::SqlValueBaseType::MiniSQL_char> charTree;
+typedef BPlusTree<MiniSQL::SqlValueBaseType::MiniSQL_float> floatTree;
 
 class IndexManager
 {
 private:
     /* data */
     BufferManager *bufferManager;
+    CatalogManager *catalogManager;
+    MiniSQL::TableInfo tableInfo;
+    MiniSQL::IndexInfo indexInfo = MiniSQL::IndexInfo("", "", "");
+    int columnIdx;
+
+    typedef std::pair<std::string, std::string> treeKey;
+    std::map<treeKey, int> intMap, charMap, floatMap;
+
+    std::vector<intTree> intTreeList;
+    std::vector<charTree> charTreeList;
+    std::vector<floatTree> floatTreeList;
+
+    MiniSQL::SqlValueBaseType getColomnType(const std::string &tableName, const std::string &columnName);
+
+    intTree& getIntTree();
+    charTree& getCharTree();
+    floatTree& getFloatTree();
+
 public:
-    IndexManager(BufferManager *_bufferManager): bufferManager(_bufferManager) {};
+    IndexManager(BufferManager *_bufferManager, CatalogManager *_catalogManager): bufferManager(_bufferManager), catalogManager(_catalogManager) {};
     ~IndexManager();
     
     bool buildIndex(const IndexInfo &index);
 
     bool dropIndex(const std::string &indexName);
 
-    int search(const std::string &filename, const Tuple &tuple);
+    int search(const std::string &tableName, const std::string &columnName, const SqlValue &value);
 
-    int searchHead(const std::string &filename, SqlValueBaseType type);
+    int serachEqual(const std::string &tableName, const std::string &columnName, const Tuple &tuple);
 
-    int searchNext(const std::string &filename, SqlValueBaseType type);
+    int searchHead(const std::string &tableName, const std::string &columnName);
 
-    bool insertKey(const std::string &filename, const Tuple &tuple, int offset);
+    int searchNext(const std::string &tableName, const std::string &columnName);
 
-    bool removeKey(const std::string &filename, const Tuple &tuple);
+    bool insertKey(const std::string &tableName, const Tuple &tuple, int offset);
+
+    // bool removeKey(const std::string &tableName, const Tuple &tuple, int offset);
 };
 
 
